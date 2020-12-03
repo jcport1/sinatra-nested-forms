b7f8f1bdb5d6ac6ab61323c026120b0c83b68671
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-12-03 16:54:21 -0500

    commit tree 031b7bf51d99cff91129ebfc695c4f839c4d9d65

--- app.rb
+++ app.rb
@@ -5,5 +5,23 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+
+      erb :root 
+
+    end 
+
+    get '/new' do
+
+      erb :new 
+
+    end 
+
+    post '/pirates' do
+
+      erb :show 
+
+    end 
+
   end
 end
